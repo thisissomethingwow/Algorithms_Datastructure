46c5b6032e2f2ba16d362dbda55753e27a68f3e4
wow <150044554+thisissomethingwow@users.noreply.github.com>
2024-11-14 21:17:02 -0330

    Update Animal.java

--- Assingment6/AnimalShelter/src/Animal.java
+++ Assingment6/AnimalShelter/src/Animal.java
@@ -1,26 +1,28 @@
 public class Animal {
     String name;
     String type;
-
-    public Animal(String name, String type) {
+    int arrival;
+    public Animal(String name, String type, int arrival) {
         this.name = name;
         this.type = type;
+        this.arrival = arrival;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getType() {
         return type;
     }
-
     public void setType(String type) {
         this.type = type;
     }
-
+    public int getArrival() {
+        return arrival;
+    }
+    public void setArrival(int arrival) {
+        this.arrival = arrival;
+    }
 }
