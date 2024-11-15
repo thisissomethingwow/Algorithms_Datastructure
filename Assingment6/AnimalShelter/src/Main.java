9417e13db7c1296fd68beb256e83cf814b0c5291
wow <150044554+thisissomethingwow@users.noreply.github.com>
2024-11-14 21:18:22 -0330

    Update Main.java

--- Assingment6/AnimalShelter/src/Main.java
+++ Assingment6/AnimalShelter/src/Main.java
@@ -2,9 +2,9 @@
 public class Main {
     public static void main(String[] args) {
         AnimalShelter animalShelter = new AnimalShelter(5);
-        Animal cat1 = new Animal("cat1","cat");
-        Animal dog1 = new Animal("dog1","dog");
-        Animal cat2 = new Animal("cat2","cat");
+        Animal cat1 = new Animal("cat1","cat",1);
+        Animal dog1 = new Animal("dog1","dog",2);
+        Animal cat2 = new Animal("cat2","cat",3);
         animalShelter.enQueue(cat1);
         animalShelter.enQueue(dog1);
         animalShelter.enQueue(cat2);
